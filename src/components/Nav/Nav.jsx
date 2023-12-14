54def554f92507d3744c87a74c2817ea8bda2682
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-12-14 17:32:52 -0500

    refactor nav component to utilize mui library

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,5 +1,18 @@
 // npm modules
 import { Link } from "react-router-dom"
+import { useState } from "react"
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import Menu from '@mui/material/Menu'
+import MenuIcon from '@mui/icons-material/Menu'
+import Container from '@mui/material/Container'
+import Button from '@mui/material/Button'
+import MenuItem from '@mui/material/MenuItem'
+import ComputerIcon from '@mui/icons-material/Computer'
+
 
 const Nav = () => {
   const gen1 = {
@@ -34,59 +47,136 @@ const Nav = () => {
     genNum: 4,
   }
 
+  const pages = [
+    gen1,
+    gen2,
+    gen3,
+    gen4,
+  ]
+
+  const [anchorElNav, setAnchorElNav] = useState(null)
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget)
+  }
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null)
+  }
+
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Link
-        to='/'
-        style={{
-          margin: '8px'
-        }}
-      >
-        Home
-      </Link>
-      <Link
-        to='/gen-i'
-        state={gen1}
-        style={{
-          margin: '8px'
-        }}
-      >
-        Kanto
-      </Link>
-      <Link
-        to='/gen-ii'
-        state={gen2}
-        style={{
-          margin: '8px'
-        }}
-      >
-        Johto
-      </Link>
-      <Link
-        to='/gen-iii'
-        state={gen3}
-        style={{
-          margin: '8px'
-        }}
-      >
-        Hoenn
-      </Link>
-      <Link
-        to='/gen-iv'
-        state={gen4}
-        style={{
-          margin: '8px'
-        }}
-      >
-        Sinnoh
-      </Link>
-    </div>
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <ComputerIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            POKÉDEX
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page.genPath} onClick={handleCloseNavMenu}>
+                  <Link
+                    to={page.genPath}
+                    state={page}
+                    style={{
+                      textDecoration: 'none',
+                      color: 'initial',
+                    }}
+                  >
+                    {page.regionName}
+                  </Link>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <ComputerIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            POKÉDEX
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Link
+                key={page.genPath}
+                to={page.genPath}
+                state={page}
+                style={{
+                  textDecoration: 'none'
+                }}
+              >
+                <Button
+                  sx={{
+                    my: 2,
+                    color: 'white',
+                    display: 'block',
+                  }}
+                >
+                  {page.regionName}
+                </Button>
+              </Link>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   )
 }
 
