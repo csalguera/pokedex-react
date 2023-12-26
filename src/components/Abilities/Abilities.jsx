d9e485d5b64fe914cb9554e57fcbbded155cf728
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-12-26 15:25:11 -0500

    add conditional render

--- src/components/Abilities/Abilities.jsx
+++ src/components/Abilities/Abilities.jsx
@@ -6,6 +6,9 @@ import Ability from "./Ability"
 import HiddenAbility from "./HiddenAbility"
 import FlexCenterWrapper from "../common/FlexCenterWrapper"
 
+// mui components
+import Typography from "@mui/material/Typography"
+
 // context
 import { PokemonDetailsContext } from "../../context/PokemonDetailsProvider"
 
@@ -15,7 +18,7 @@ const Abilities = () => {
     pokemonDetails,
   } = useContext(PokemonDetailsContext)
 
-  if (currentGen < 3) return
+  if (currentGen < 3) return <Typography>No Abilities</Typography>
   return (
     <>
       {pokemonDetails?.abilities?.map(ability => (
