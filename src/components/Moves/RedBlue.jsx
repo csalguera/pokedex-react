b381c445244cf466da0c52b90ce1f0e2012fa66b
Carlos Salguera <carlos.e.salguera@gmail.com>
2024-01-03 16:56:55 -0500

    remove unused modules

--- src/components/Moves/RedBlue.jsx
+++ src/components/Moves/RedBlue.jsx
@@ -7,7 +7,6 @@ import ListItemTextWrapper from "../common/ListItemTextWrapper"
 // mui components
 import List from "@mui/material/List"
 import ListItem from "@mui/material/ListItem"
-import ListItemText from "@mui/material/ListItemText"
 import Divider from "@mui/material/Divider"
 
 // utilities
@@ -16,7 +15,6 @@ import { removeHyphens } from "../../utilities/utilities"
 // context
 import { PokemonMovesContext } from "../../context/MovesProvider"
 import { PokemonDetailsContext } from "../../context/PokemonDetailsProvider"
-import FlexCenterWrapper from "../common/FlexCenterWrapper"
 
 const RedBlue = () => {
   const {
